f22b174abaa45519afebff181d61900f055cbb88
Grégory Witmer <47319614+Greg8052@users.noreply.github.com>
2023-06-01 12:44:28 +0200

    Make Path stay highlighted

--- demo/demo.js
+++ demo/demo.js
@@ -211,7 +211,7 @@ GraphSearch.prototype.animatePath = function(path) {
         if(i >= path.length) { // finished removing path, set start positions
             return setStartClass(path, i);
         }
-        elementFromNode(path[i]).removeClass(css.active);
+        // elementFromNode(path[i]).removeClass(css.active);
         setTimeout(function() {
             removeClass(path, i+1);
         }, timeout*path[i].getCost());
